4f8f48ab06fad3d2f3ab243cf344c10f6d114ca9
Connor Haggarty <connor.c.haggarty@bbc.co.uk>
2026-07-14 15:00:34 +0100

    Added homepage redesign and battleship styling

--- ksb-battleships/app/page.tsx
+++ ksb-battleships/app/page.tsx
@@ -15,13 +15,15 @@ if (gameStarted) {
 }
 
 return (
-  <main className="flex flex-col items-center justify-center min-h-screen p-4">
-    <h1 className="text-6xl md:text-7xl font-extrabold text-slate-900 p-10">KSBattleships</h1>
-    <div>
-      <h2 className="text-2xl font-bold text-slate-700"> Instructions </h2>
-      <p className="text-slate-600 text-center">...</p>
+  <main className="bg-offblack flex flex-col items-center justify-center min-h-screen p-4">
+    <h1 className="text-teal text-6xl md:text-7xl font-extrabold tracking-tight p-10 font-heading">
+      KSBattleships
+    </h1>
+    <div className="text-teal font-heading">
+      <h2 className="text-2xl font-bold">Instructions</h2>
+      <p className="text-center">...</p>
     </div>
-    <button className="text-3xl font-bold text-slate-700 bg-slate-200 hover:bg-slate-300 p-4 rounded-lg">
+    <button className="text-3xl font-bold text-slate-700 bg-offwhite hover:bg-slate-300 p-4 rounded-lg font-heading border-teal border-2">
       Start Game
     </button>
   </main>
